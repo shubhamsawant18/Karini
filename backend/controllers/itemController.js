06ec0735d797221d7480526f381134a9755cc405
shubhamsawant18 <shubhsawant124@gmail.com>
2025-03-06 15:26:59 +0530

    item controller updated

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -10,18 +10,48 @@ const postProduct = async(req, res)=>{
     }
 }
 
-const getProducts = async(req,res)=>{
+const getProducts = async (req, res) => {
     try {
-        const products = await Product.find();
+        const { search } = req.query;
+        let query = {};
+
+        if (search) {
+            query = {
+                $or: [
+                    { Handle: { $regex: search, $options: "i" } },
+                    { Title: { $regex: search, $options: "i" } },
+                    // { Body: { $regex: search, $options: "i" } },
+                    { Vendor: { $regex: search, $options: "i" } },
+                    { Type: { $regex: search, $options: "i" } },
+                    { Tags: { $regex: search, $options: "i" } },
+                    { "Option1 Name": { $regex: search, $options: "i" } },
+                    { "Option1 Value": { $regex: search, $options: "i" } },
+                    { "Option2 Name": { $regex: search, $options: "i" } },
+                    { "Option2 Value": { $regex: search, $options: "i" } },
+                    { "Option3 Name": { $regex: search, $options: "i" } },
+                    { "Option3 Value": { $regex: search, $options: "i" } },
+                    { "Variant SKU": { $regex: search, $options: "i" } },
+                    { "Variant Inventory Tracker": { $regex: search, $options: "i" } },
+                    { "Variant Inventory Policy": { $regex: search, $options: "i" } },
+                    { "Variant Fulfillment Service": { $regex: search, $options: "i" } },
+                    { "Variant Price": { $regex: search, $options: "i" } },
+                    { "Variant Compare At Price": { $regex: search, $options: "i" } },
+                ],
+            };
+        }
+
+        const products = await Product.find(query);
+
         res.json({
             error: false,
-            messsage: 'Data fetched successfully',
+            message: 'Data fetched successfully',
             data: products
         });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-}
+};
+
 
 const updateProduct = async(req,res)=>{
     try {
